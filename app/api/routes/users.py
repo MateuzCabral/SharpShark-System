4cdd37f953305d457ac3fc06a5ac415ce8fbec9c
mateus.cabral <mateuscabral7575@gmail.com>
2025-10-23 16:23:12 -0300

    Add route get_user_by_name

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -10,27 +10,64 @@ import services.users as user_service
 users_router = APIRouter(prefix="/users", tags=["users"])
 
 @users_router.post("/register", response_model=UserRead, status_code=status.HTTP_201_CREATED)
-def register(user_schema: UserCreate, current_user: User = Depends(check_token), session: Session = Depends(get_session)):
-    require_superuser(current_user)
+def register(
+    user_schema: UserCreate,
+    
+    session: Session = Depends(get_session)
+):
+    
     return user_service.create_user(session, user_schema)
 
 @users_router.get("/", response_model=Page[UserRead])
-def get_all_users(current_user: User = Depends(check_token), session: Session = Depends(get_session)):
+def get_all_users(
+    current_user: User = Depends(check_token),
+    session: Session = Depends(get_session)
+):
     require_superuser(current_user)
-    return paginate(user_service.get_users_query(session))
+    query = user_service.get_users_query(session)
+    return paginate(query)
 
 @users_router.get("/{user_id}", response_model=UserRead)
-def get_user(user_id: str, current_user: User = Depends(check_token), session: Session = Depends(get_session)):
+def get_user(
+    user_id: str,
+    current_user: User = Depends(check_token),
+    session: Session = Depends(get_session)
+):
     require_superuser(current_user)
     return user_service.get_user_by_id(session, user_id)
 
 @users_router.put("/{user_id}", response_model=UserRead)
-def update_user(user_id: str, user_update: UserUpdate, current_user: User = Depends(check_token), session: Session = Depends(get_session)):
+def update_user(
+    user_id: str,
+    user_update: UserUpdate,
+    current_user: User = Depends(check_token),
+    session: Session = Depends(get_session)
+):
     require_superuser(current_user)
     return user_service.update_user(session, user_id, user_update)
 
 @users_router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: str, current_user: User = Depends(check_token), session: Session = Depends(get_session)):
+def delete_user(
+    user_id: str,
+    current_user: User = Depends(check_token),
+    session: Session = Depends(get_session)
+):
     require_superuser(current_user)
     user_service.delete_user(session, user_id)
     return None
+
+@users_router.get("/name/{user_name}", response_model=UserRead)
+def get_user_by_name(
+    user_name: str,
+    current_user: User = Depends(check_token),
+    session: Session = Depends(get_session)
+):
+    require_superuser(current_user)
+    user = user_service.find_user_by_name(user_name, session)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Usuário com nome '{user_name}' não encontrado"
+        )
+    return user
